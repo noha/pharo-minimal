6ca83c227c358519d95fe4947dc52dc9bf4cdeeb
Norbert Hartl <norbert@hartl.name>
2019-03-11 18:38:14 +0100

    removed unnecessary file

--- server/build-kikimatic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp telepharo.image kiki-matic.image
-cp telepharo.changes kiki-matic.changes
-
-./pharo kiki-matic.image eval --save "Metacello new baseline: 'UnifiedFFI';repository: 'github://pharo-project/pharo:Pharo8.0/src'; load"
-
-./pharo kiki-matic.image eval --save "Metacello new repository: 'filetree://source'; baseline: 'KikiMatic'; onConflictUseLoaded; load: #( 'server' )"
