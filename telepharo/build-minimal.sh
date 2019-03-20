9077d1d993d9160f1493c4478beeaeb88dcd8727
Norbert Hartl <norbert@hartl.name>
2019-03-20 15:16:12 +0100

    changed tonel version

--- telepharo/build-minimal.sh
+++ telepharo/build-minimal.sh
@@ -5,7 +5,7 @@ cp minimal-pharo.changes minimal.changes
 
 ../bin/pharo minimal.image eval --save "ZnMimeType initialize"
 
-../bin/pharo minimal.image  eval --save "Metacello new baseline: 'Tonel';repository: 'github://pharo-vcs/tonel:v1.0.9'; load: 'core'"
+../bin/pharo minimal.image  eval --save "Metacello new baseline: 'Tonel';repository: 'github://pharo-vcs/tonel:v1.0.13'; load: 'core'"
 
 ../bin/pharo minimal.image eval --save "Metacello new baseline: 'SUnit';repository: 'github://pharo-project/pharo:Pharo8.0/src'; load"
 
